60f59028820d8de75131d43d6f29a158dc6e5a63
Chris P <chrisp1877@gmail.com>
2020-12-15 21:01:39 -0800

    changing adapter to netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  adapter: "@sveltejs/adapter-netlify",
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: '@sveltejs/adapter-static'
+		adapter: '@sveltejs/adapter-netlify'
 	}
 };
